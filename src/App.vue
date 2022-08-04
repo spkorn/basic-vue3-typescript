809917d154107cebd97d58a026700dbd32e84fc1
Supakorn Meelarp <supakorn.m@muze.co.th>
2022-08-04 13:46:02 +0700

    [fixed] add count2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <h1>App.vue</h1>
-    <span>#Debug: {{count1}}</span>
+    <span>#Count1: {{ count1 }}</span> <br />
+    <span>#Count2: {{ count2 }}</span> <br />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
+    let count1 = 11;
+    const count2 = ref<number>(0);
+
     return {
-      count1: 10
-    }
+      count1,
+      count2,
+    };
   },
-})
+});
 </script>
