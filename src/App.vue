55774a2d401f2c91963bc583e76e91fa4b2008eb
Supakorn Meelarp <supakorn.m@muze.co.th>
2022-08-07 19:29:49 +0700

    add Add button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <h1>App.vue</h1>
     <span>#Count1: {{ count1 }}</span> <br />
     <span>#Count2: {{ count2 }}</span> <br />
+    <button @click="onClickAdd1">Add1</button>
+    <button @click="onClickAdd2">Add2</button>
   </div>
 </template>
 
@@ -12,11 +14,23 @@ import { defineComponent, ref } from "vue";
 export default defineComponent({
   setup() {
     let count1 = 11;
-    const count2 = ref<number>(0);
+    const count2 = ref<number>(2);
+
+    const onClickAdd1 = () => {
+      count1 = count1 + 1;
+      console.log("count1 = " + count1);
+    };
+
+    const onClickAdd2 = () => {
+      count2.value = count2.value + 1;
+      console.log("count2 = " + count2.value);
+    };
 
     return {
       count1,
       count2,
+      onClickAdd1,
+      onClickAdd2,
     };
   },
 });
